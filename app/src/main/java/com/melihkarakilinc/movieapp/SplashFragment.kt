4e4f27b68d24a5324fa3d80dc731a68da2cc13da
melihkarakilinc <57638333+melihkarakilinc@users.noreply.github.com>
2022-01-13 20:09:13 +0300

    #Add Splash Screen

---
+++ app/src/main/java/com/melihkarakilinc/movieapp/SplashFragment.kt
@@ -0,0 +1,36 @@
+package com.melihkarakilinc.movieapp
+
+import android.os.Bundle
+import android.os.Handler
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+
+
+class SplashFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_splash, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        Handler(requireContext().mainLooper).postDelayed({
+            val direction = SplashFragmentDirections
+                .actionSplashFragmentToAllMovieFragment()
+            findNavController().navigate(direction)
+        }, 3000)
+    }
+
+}
