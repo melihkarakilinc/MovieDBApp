ba81c827356d294cc788beb1137fbae9dfa6e8ad
melihkarakilinc <57638333+melihkarakilinc@users.noreply.github.com>
2022-01-18 20:16:33 +0300

    #Add ApiUrl object

---
+++ app/src/main/java/com/melihkarakilinc/movieapp/Utils/ApiUrl.kt
@@ -0,0 +1,9 @@
+package com.melihkarakilinc.movieapp.Utils
+
+object ApiUrl {
+    const val BASE_URL = "https://api.themoviedb.org/"
+    const val API_KEY = "ApiKey"
+    const val MOVIE_URL = "3/movie/popular"
+    const val POSTER_PATH="https://image.tmdb.org/t/p/original/"
+    const val SIMILAR_MOVİE="3/movie/popular"
+}
